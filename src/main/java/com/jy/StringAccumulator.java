2a14adc97fd2c71de95f06483ddad08ede415ff2
Jeffrey Yip <jeffreyyip@Jeffreys-MacBook-Pro.local>
2019-09-27 01:33:14 +0800

    3rd commit
    3rd commit

--- src/main/java/com/jy/StringAccumulator.java
+++ src/main/java/com/jy/StringAccumulator.java
@@ -2,53 +2,59 @@ package com.jy;
 
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringAccumulator {
 
     private static final String COMMAS = ",";
     private static final String RETURN = "\n";
     private static final String[] DEFAULTS_DELIMITORS = {COMMAS, RETURN};
-    /*
-    private String delimiterAndNumbers;
-    private String[] delimitors;
-    private String[] numbers;
-    */
-
+    private static final String DELIMITERS_PREFIX = "//";
+    private static final String DELIMITERS_SUFFIX = "\n";
+    private static final String DELIMITERS_SEPARATOR = "|";
+    private static final String REGEX_OR = "|";
 
 
     private static String[] parseDelimitors(String delimiterAndNumbers){
-        // delimiter format: //del1|del2|del3\n
+        // delimiter format: //del1|del2|del3\n<numbers>
 
-        int newLineIndex = delimiterAndNumbers.indexOf("\n");
-        String delimitors = delimiterAndNumbers.substring(2, newLineIndex);
-        String[] token = delimitors.split("\\|");
-        printArray(token);
-        return token;
+        int newLineIndex = delimiterAndNumbers.indexOf(DELIMITERS_SUFFIX);
+        String delimiterStr = delimiterAndNumbers.substring(DELIMITERS_PREFIX.length() , newLineIndex);
+
+        return delimiterStr.split(escapeRegex(DELIMITERS_SEPARATOR));
+
+
     }
 
-    private static String[] parseNumber(String delimitedNumber, String[] delimitors){
+    private static String[] parseNumber(String delimitedNumber, String[] delimitors)  {
+
+        return delimitedNumber.split(String.join(REGEX_OR, escapeRegex(delimitors)));
 
 
-        String[] strings =  delimitedNumber.split(String.join("|", escapeRegex(delimitors)));
+    }
 
+    private static void validateNegativeNumbers(String[] numbers) throws Exception {
+        List<String> negativeNumbers = Arrays.stream(numbers).filter(x -> x.startsWith("-")).collect(Collectors.toList());
 
-        return strings;
+        if (negativeNumbers.size() > 0 ){
+            throw new Exception("negatives not allowed - " + negativeNumbers);
+        }
     }
 
     private static String[] escapeRegex(String[] delimiters){
-        String[] escapeDelimiters = new String[delimiters.length];
 
-        for (int i = 0; i < delimiters.length; i++) {
-            escapeDelimiters[i] = escapeRegex(delimiters[i]);
-        }
+        return Arrays.stream(delimiters).map(StringAccumulator::escapeRegex).toArray(String[]::new);
 
-        return escapeDelimiters;
     }
+
     private static String escapeRegex(String delimiter){
-        StringBuffer sb = new StringBuffer();
+
+
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < delimiter.length(); i++){
-            if (delimiter.charAt(i) == '*'){
+            if (delimiter.charAt(i) == '*' || delimiter.charAt(i) == '|'){
                 sb.append("\\").append(delimiter.charAt(i));
             } else {
                 sb.append(delimiter.charAt(i));
@@ -56,7 +62,11 @@ public class StringAccumulator {
         }
 
         return sb.toString();
+
+
     }
+
+    /*
     private static void printArray(String[] strs){
         System.out.println(String.format("length: %s", strs.length));
         for (String s: strs){
@@ -64,35 +74,51 @@ public class StringAccumulator {
         }
     }
 
+     */
+
     private static String removeDelimitors(String delimiterAndNumbers){
-        int newlineIndex = delimiterAndNumbers.indexOf("\n");
+
+        int newlineIndex = delimiterAndNumbers.indexOf(DELIMITERS_SUFFIX);
         return delimiterAndNumbers.substring(newlineIndex+1);
     }
 
-    private static int sum(String[] numbers){
-        printArray(numbers);
+    private static int sumIgnoreOverThousand(String[] numbers){
+        //System.out.println("=== sumStream ===");
 
+        return Arrays.asList(numbers).parallelStream().mapToInt( Integer::parseInt).filter( i -> i <= 1000).sum();
+    }
+
+    /*
+    private static int sumLoop(String[] numbers){
+        System.out.println("=== sumLoop ===");
         int result = 0;
+        int n;
 
         for (String s : numbers){
-            result += Integer.parseInt(s);
+             n = Integer.parseInt(s);
+
+             if (n <= 1000 ){
+                 result += n;
+             }
         }
 
         return result;
     }
 
+     */
+
     private static boolean startWithDelimiters(String delimiterAndNumbers){
-        return delimiterAndNumbers.startsWith("//") && delimiterAndNumbers.contains("\n");
+        return delimiterAndNumbers.startsWith(DELIMITERS_PREFIX) && delimiterAndNumbers.contains(DELIMITERS_SUFFIX);
     }
 
-    public static int add(String delimiterAndNumbers){
+    public static int add(String delimiterAndNumbers) throws Exception {
         String[] delimiters;
         String numberStr;
 
         if (delimiterAndNumbers.length() == 0)
             return 0;
 
-        if (startWithDelimiters(delimiterAndNumbers)){
+        if (startWithDelimiters(delimiterAndNumbers)) {
             delimiters = parseDelimitors(delimiterAndNumbers);
             numberStr = removeDelimitors(delimiterAndNumbers);
         } else {
@@ -102,17 +128,22 @@ public class StringAccumulator {
 
         String[] numbers = parseNumber(numberStr, delimiters);
 
+        validateNegativeNumbers(numbers);
 
-
-        return sum(numbers);
+        return sumIgnoreOverThousand(numbers);
     }
 
 
     public static void main(String[] args){
 
+        try {
+            int result = StringAccumulator.add(args[0]);
 
-        int result = StringAccumulator.add(args[0]);
+            System.out.println(String.format("result: %s", result));
 
-        System.out.println(String.format("result: %s", result));
+        }catch (Exception e){
+            e.printStackTrace(System.out);
+        }
+
     }
 }
--- src/test/java/com/jy/StringAccumulatorTest.java
+++ src/test/java/com/jy/StringAccumulatorTest.java
@@ -5,8 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -21,90 +20,125 @@ public class StringAccumulatorTest {
     }
 
     @Test
-    public void add_zero_param() {
+    public void add_zero_number() throws Exception {
         int result = StringAccumulator.add("");
         assertEquals(0, result);
     }
 
     @Test
-    public void add_one_param() {
+    public void add_one_number()  throws Exception {
         int result = StringAccumulator.add("1");
         assertEquals(1, result);
     }
 
     @Test
-    public void add_two_params_commas() {
+    public void add_two_numbers()  throws Exception {
         int result = StringAccumulator.add("1,2");
         assertEquals(3, result);
     }
 
     @Test
-    public void add_three_params_commas() {
+    public void add_three_numbers()  throws Exception {
         int result = StringAccumulator.add("1,2,3");
         assertEquals(6, result);
     }
 
+
     @Test
-    public void add_unknown_params_commas() {
-        int start = 1;
-        int end = 65000;
-        IntStream intStream = IntStream.range(start, end+1);
-        String numbersStr = intStream.mapToObj(String::valueOf).collect(Collectors.joining(","));
-        long startTime = System.currentTimeMillis();
-        int result = StringAccumulator.add(numbersStr);
-        System.out.println(System.currentTimeMillis() - startTime);
-        assertEquals((end/2)*(end+1), result);
+    public void add_unknown_numbers()  throws Exception {
+        int cnt = 100000;
+        String[] numbers = new String[cnt];
+
+        Arrays.fill(numbers, "1");
+
+
+        //long startTime = System.currentTimeMillis();
+
+        int result = StringAccumulator.add(String.join(",", numbers));
+
+        //System.out.println(String.format("time taken: %s", System.currentTimeMillis() - startTime));
+        assertEquals(cnt, result);
     }
 
     @Test
-    public void add_two_params_newline() {
+    public void add_two_numbers_newline()  throws Exception {
         int result = StringAccumulator.add("1\n2");
         assertEquals(3, result);
     }
 
 
     @Test
-    public void add_three_params_newline() {
+    public void add_three_numbers_newline()  throws Exception {
         int result = StringAccumulator.add("1\n2\n3");
         assertEquals(6, result);
     }
 
     @Test
-    public void add_three_params_commas_newline() {
+    public void add_three_numbers_commas_newline()  throws Exception {
         int result = StringAccumulator.add("1\n2,3");
         assertEquals(6, result);
     }
 
     @Test
-    public void add_different_delimiters_semi() {
+    public void add_with_delimiter_semicolon()  throws Exception {
         int result = StringAccumulator.add("//;\n1;2");
         assertEquals(3, result);
     }
 
-    @Test
-    public void add_different_delimiters_multipleLength() {
-        int result = StringAccumulator.add("//***\n1***2");
-        assertEquals(3, result);
+    @Test(expected = Exception.class)
+    public void add_negatives_then_exception_message()  throws Exception {
+        String negative1 = "-2";
+        String negative2 = "-4";
+
+        try{
+            StringAccumulator.add("//;\n1;" + negative1 + ";3;" + negative2);
+
+        }catch(Exception e){
+
+
+            assertTrue(e.getMessage().startsWith("negatives not allowed"));
+            assertTrue(e.getMessage().contains(negative1));
+            assertTrue(e.getMessage().contains(negative2));
+
+            throw e;
+        }
     }
 
     @Test
-    public void add_different_delimiters_semi_star() {
-        int result = StringAccumulator.add("//;|%\n1;2%3");
+    public void add_ignore_greater_thousand() throws Exception {
+
+        int result = StringAccumulator.add("2,1001,3,2001");
+        assertEquals(5, result);
+
+    }
+
+    @Test
+    public void add_with_delimiter_multipleLength()  throws Exception {
+        int result = StringAccumulator.add("//***\n1***2***3");
         assertEquals(6, result);
     }
 
     @Test
-    public void add_different_delimiters_semi_perc_star() {
-        int result = StringAccumulator.add("//;|%|***\n1;2%3***4");
-        assertEquals(10, result);
+    public void add_multiple_delimiters()  throws Exception {
+        int result = StringAccumulator.add("//*|%\n1*2%3");
+        assertEquals(6, result);
     }
 
     @Test
+    public void add_multiple_delimiters_multiple_chars()  throws Exception {
+        int result = StringAccumulator.add("//***|%|*\n1*2%3***4");
+        assertEquals(10, result);
+    }
+
+    /*
+    @Test
     public void test_split(){
-        String str = "1**2,3";
-        String[] token = str.split("\\*\\*|,");
+        String str = "1*2%3***4";
+        String[] token = str.split("\\*\\*\\*|%|\\*");
         for (String t : token){
             System.out.println(t);
         }
     }
+
+     */
 }
